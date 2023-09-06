f634d4da6cd9ff470c5c9ea83e126bbc1ef8c3e7
Angel Magaquian <angelmagakhian@gmail.com>
2023-09-05 23:38:09 -0300

    variables

---
+++ jsBasico/variablesEif.js
@@ -0,0 +1,23 @@
+/*Declaración variables*/
+
+//var valor1 = 0
+let valor2 = -1
+const valor3 = 3.5
+
+/*String*/
+
+let cadena = "cadena con comillas simples"
+let cadena2 = 'cadena con comillas simples'
+let cadena3 = `sirve para escribir una cadena ${valor2 * valor3}`
+
+/*Booleanos*/
+
+let booleano = true
+let boolean = false
+
+console.log(cadena3)
+
+
+
+
+
