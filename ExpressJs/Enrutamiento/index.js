d80322ef92637c6c83b17040851f97f372806be4
Angel Magaquian <angelmagakhian@gmail.com>
2023-10-17 21:00:42 -0300

    enrutamiento 2

--- ExpressJs/Enrutamiento/index.js
+++ ExpressJs/Enrutamiento/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { readFile, writeFile  } from 'fs/promises' /*Codigo nuevo */
 
 const app = express()
 
@@ -10,6 +11,9 @@ app.listen(port, () =>{
     console.log(`Servidor levantado en puerto ${port}`)
 })
 
+
+const file = await readFile('./data.json', 'utf-8') /*Codigo nuevo */
+const userData = JSON.parse(file) /*Codigo nuevo */
 
 const objetos = [
     {name: 'Auto', color: 'Rojo'},
@@ -45,4 +49,47 @@ app.post('/ColorDePost', (req,res)=>{
         res.status(404).json(`${obj} no se encuentra`)
     }
 })
+
+
+app.get('/users/all', (req,res)=>{
+    res.status(200).json(userData)
+}) /*Codigo nuevo */
+
+app.put('/users/salary/update/:userID', (req,res)=>{
+    const user_id = req.params.userID
+    const new_salary = req.body.salary
+
+    try{
+        const index = userData.findIndex(e => e.id == user_id)
+        if(index != -1){
+            userData[index].salary = new_salary
+            writeFile('./data.json', JSON.stringify(userData,null,2));
+            res.status(200).json('Salario modificado')
+        }else{
+            res.send(400).json('No se encontro al usuario')
+        }
+    }catch(error){
+        res.send(500).json('Error al actualizar el salario')
+    }
+})
+
+
+app.delete('/users/delete/:userID',(req,res)=>{
+    const user_id = req.params.userID
+
+    try{
+        const index = userData.findIndex(e => e.id == user_id)
+
+        if(index !== -1){
+            userData.splice(index,1)
+            writeFile('./data.json', JSON.stringify(userData,null,2));
+            res.status(200).json('Usuario Eliminado')
+        }else{
+            res.send(400).json('No se encontro al usuario')
+        }
+    }catch(error){
+        res.send(500).json('Error al eliminar usuario')
+    }
+})
+
 
