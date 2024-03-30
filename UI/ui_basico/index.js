6524da8ceef57e0b45b4a547f6dd4c8864a897b9
Angel Magaquian <angelmagakhian@gmail.com>
2024-03-30 17:44:32 -0300

    conexion con ui basico

---
+++ UI/ui_basico/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import userRouter from './routes/user.routes.js'
+
+const app = express()
+
+const port = 3000
+
+app.use(express.json())
+
+app.listen(port,()=>{
+    console.log(`Servidor levantado en el puerto ${port}`)
+})
+
+/*PARA LEVANTAR NUESTRO FRONT-END */
+app.use(express.static('./public'))
+
+
+/*RUTAS DE END-POINT */
+app.use('/user', userRouter)
