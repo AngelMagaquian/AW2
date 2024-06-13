918773ba4e3e9d5a6edcdf2de049ced2e45e90a2
Angel Magaquian <angelmagakhian@gmail.com>
2024-06-12 21:18:55 -0300

    mongoDB

---
+++ mongoDb/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import productRoutes from './routes/productos.routes.js'
+import salesRoutes from './routes/ventas.routes.js'
+import categoryRoutes from './routes/category.routes.js'
+import 'dotenv/config'
+
+const app = express()
+app.use(express.json())
+
+const port = process.env.PORT
+
+app.listen(port, () =>{
+    console.log(`Servidor levantado en puerto ${port}`)
+})
+
+
+app.use('/products', productRoutes)
+app.use('/sales', salesRoutes)
+app.use('/categories', categoryRoutes)
